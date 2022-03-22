b6521e33c6deff7d138620dcebc3b01011966f4a
Ruski1 <adam882200@gmail.com>
2022-03-22 22:23:29 +0000

    Initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import "antd/dist/antd.css";
+import { Calendar } from 'antd';
+import moment from 'moment';
+
+var FirstPick = moment();
+var SecondPick = moment();
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div style={{
+			display: 'block', width: 700, padding: 30
+		}}>
+			<h4>ReactJS Ant-Design Calendar Component</h4>
+			<Calendar onChange={(value) =>
+			{
+				
+				if (FirstPick != null)
+				{
+					FirstPick = value
+					console.log(FirstPick)
+					alert(FirstPick)
+				}
+				else
+				{
+					//FirstPick = value;
+					SecondPick = value
+					//alert(`Range is ${FirstPick.valueOf() - SecondPick.valueOf()}`)
+					alert(FirstPick)
+					alert(SecondPick)
+					console.log(FirstPick)
+					console.log(SecondPick)
+				}
+
+			}} />
+		</div>
+	);
 }
 
 export default App;
